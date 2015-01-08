2178469bc6df2a2310b0c64ce03f2ffb426cc195
shirokane <jcanete@aoiths.org>
2015-01-07 19:45:33 -0500

    model change

---
+++ models/model.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+require 'json'
+require 'flickr.rb'
+require 'pry'
+
+countries = ["china","mexico","switzerland","greenland","yemen"]
+
+
+# flickr = Flickr.new('1e9ca4287cf30cf0fff691d5970fdd21')
+# puts flickr.photos.first
+
+
+
+uri = open('https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=1e9ca4287cf30cf0fff691d5970fdd21&tags=china&format=json')
+list = Net::HTTP.get(uri)
+binding.pry
+jlist = JSON.parse(list)
+
+puts jlist
