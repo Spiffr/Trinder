0b51a6f0c032db06fbb69064cdc89cf2933a58d3
dfenjves <fenjves.daniel@gmail.com>
2015-01-21 14:03:56 -0500

    made changes to the model

--- models/model.rb
+++ models/model.rb
@@ -2,41 +2,49 @@ require 'net/http'
 require 'json'
 require 'flickr.rb'
 require 'pry'
-countries = ["Brazil","China","South Africa","Bahamas","Germany"]
+require_relative './query'
+
+class Picture
+    def initialize
+        new_query = Query.new
+    # uri = open('https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=1e9ca4287cf30cf0fff691d5970fdd21&' + @query.to_s)
+        uri = URI('https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=1e9ca4287cf30cf0fff691d5970fdd21&' + new_query.show)
+        list = Net::HTTP.get(uri)
+                binding.pry
+        jlist = JSON.parse(list)
+
+
+        # https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+        pic = jlist.photo.rand
+        id = pic.id
+        farm = pic.farm
+        secret = pic.secret
+        server = pic.server
+        puts "farm: #{farm} server: #{server} id: #{id} secret: #{secret}"
+        picurl = "https://farm#{farm}.staticflickr.com/#{server}/#{id}_#{secret}.jpg"
+        picurl
+    end
+
+end
 
 # flickr = Flickr.new('1e9ca4287cf30cf0fff691d5970fdd21')
 # puts flickr.photos.first
 # https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=1e9ca4287cf30cf0fff691d5970fdd21&tags=china&format=json
 
 
-def picture
-    # uri = open('https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=1e9ca4287cf30cf0fff691d5970fdd21&' + @query.to_s)
-    uri = open('https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=1e9ca4287cf30cf0fff691d5970fdd21&tags=china&format=json')
 
-    list = Net::HTTP.get(uri)
-    jlist = JSON.parse(list)
-    # https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-    pic = jlist.photo.rand
-    id = pic.id
-    farm = pic.farm
-    secret = pic.secret
-    server = pic.server
-    puts "farm: #{farm} server: #{server} id: #{id} secret: #{secret}"
-    picurl = "https://farm#{farm}.staticflickr.com/#{server}/#{id}_#{secret}.jpg"
-    puts picurl
-end
 
-if countries[0] === true
-    brazil_point
-elsif countries[1] === true
-    china_point
-elsif countries[2] === true
-    south_africa_point
-elsif countries[3] === true
-    bahamas_point
-elsif countries[4] === true
-    germany_point
-end
+# if countries[0] === true
+#     brazil_point
+# elsif countries[1] === true
+#     china_point
+# elsif countries[2] === true
+#     south_africa_point
+# elsif countries[3] === true
+#     bahamas_point
+# elsif countries[4] === true
+#     germany_point
+# end
 
-picture()
+p = Picture.new
 
--- models/query.rb
+++ models/query.rb
@@ -1,14 +1,14 @@
 class Query
 	
 	def initialize
-		countries = ["Brazil","China","South Africa","Bahamas","Germany"]
+		countries = ["Brazil","China","South_Africa","Bahamas","Germany"]
 		type = ["food","landscape","culture"]
 		@combine = countries.sample + "+" + type.sample 
 	end
 
 	def show
 		@query = "tags=" + @combine + "&format=json"
-		puts @query
+		@query
 	end
 
 end 
